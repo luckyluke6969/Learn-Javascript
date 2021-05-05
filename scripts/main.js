a4d89b16ce88a848883961f572e50ba837e5b6a7
Luca Granato <lucaggranato@gmail.com>
2021-05-06 00:15:32 +0200

    BASIC LOOPS AND ARRAYS, FOR EACH

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-
+/*
 $('#addOne').hover( function() {
     var value = $('#name').val();
     $('ul').append('<li>' + value + '</li>');
     $('ul li:eq(0)').remove();
     $('#footer').toggle();
 });
+*/
 
+/* 
 function go(name, age) {
     if(age<21) {
         return name;
@@ -18,11 +20,47 @@ function go(name, age) {
 }
 
 alert( go('Luke', 24))
+*/
 
 });
 
+// FOR EACH // 
+/*
+const myList = ['apples', 'oranges', 'bananas'];
 
+console.log(myList[1]);
 
+myList.forEach(function(value, index) {
+ alert('I have ' + value + ' in my shopping bag');
+});
+*/
+
+// WHILE LOOP //
+
+var times = 0;
+/*
+while (times<10) {
+  console.log('tried it', times);
+  times++;
+}
+*/
+
+// DO LOOP // 
+/*
+do {
+    console.log('tried it', times);
+    times++;
+} while(times<10);
+*/
+
+//FOR LOOP//
+/*
+var myList = ['apples', 'oranges', 'bananas'];
+
+for (var i=0; i < myList.length; i++) {
+    alert('you have '+myList[i]+' in your basket');
+}
+/*
 
 
 
