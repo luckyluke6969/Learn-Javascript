dcdef05b4669980f2006f8e6ae66571fe8a506ef
Luca Granato <lucaggranato@gmail.com>
2021-05-05 23:38:47 +0200

    truly html before js, cool functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,32 @@
-alert('hey');
+$(document).ready(function() {
+
+$('#addOne').hover( function() {
+    var value = $('#name').val();
+    $('ul').append('<li>' + value + '</li>');
+    $('ul li:eq(0)').remove();
+    $('#footer').toggle();
+});
+
+function go(name, age) {
+    if(age<21) {
+        return name;
+    } else if(age==21) {
+   return name + '!';
+    } else {
+        return age;
+    }
+}
+
+alert( go('Luke', 24))
+
+});
+
+
+
+
+
+
+
 
 
 
